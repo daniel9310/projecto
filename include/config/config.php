5b3b4c2c13d24992d02e75b6d213c0599cb4f58e
danielmendez1393 <danielmendez_13@hotmail.com>
2016-10-13 11:57:58 -0500

    asd

--- include/config/config.php
+++ include/config/config.php
@@ -3,9 +3,9 @@ ini_set('default_charset','UTF-8');
 $_SERVER['HTTP_ACCEPT_CHARSET'] = "UTF-8";
 $charset = "UTF-8";
 error_reporting (E_ALL ^ E_NOTICE ^ E_DEPRECATED);
-$CONFIG['path'] = "c://xampp/htdocs/projecto/";
-$CONFIG['sitio'] = "http://".$_SERVER['HTTP_HOST']."/projecto/";
-$CONFIG['pathinclude'] = "c://xampp/htdocs/projecto/include/";
+$CONFIG['path'] = "c://xampp/htdocs/ISTAI/projecto/";
+$CONFIG['sitio'] = "http://".$_SERVER['HTTP_HOST']."/ISTAI/projecto/";
+$CONFIG['pathinclude'] = "c://xampp/htdocs/ISTAI/projecto/include/";
 $CONFIG['sitioimgnotas'] = $CONFIG['sitio']."files/notas/img/";
 
 $CONFIG['pathpuente'] = "/home/itiesorg/public_html/puente/";
