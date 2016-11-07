4d1912e45fa918ab293c1fea287d981335ae3929
daniel mendez robles <danielmendez93@gmail.com>
2016-11-07 10:19:32 -0700

    Panel Admin campo

--- Admin/Trans/crud/campo/create.php
+++ Admin/Trans/crud/campo/create.php
@@ -1,1 +1,35 @@
-create.php
+<?php 
+    session_start();
+    require_once("../../../../include/config/config.php");
+    require_once($CONFIG['pathinclude']."config/cx.php");
+    require_once($CONFIG['pathinclude']."cls/transparencia.php");
+    
+ ?>
+<div >
+              <h3></h3>
+                  <form class="form" target="updh"  method="POST"  action="<?=$CONFIG['pathtrans']?>Fracmodificar.php">
+                        <input type="hidden" name="frac_id" value="<?=$UpFracc['id_frac']?>">
+                        
+                        <div class="input-field col s12">
+                          <input name="fracnum"  type="number" class="validate" value="<?=$UpFracc['num_frac']?>" required >
+                          <label for="fracnum">Numero de Fraccion</label>
+                        </div> 
+
+                        <div class="input-field col s12">
+                            <i class="material-icons prefix">note</i>
+                            <textarea id="icon_prefix2" class="materialize-textarea" name="fraccdesc" length="120" required><?=$UpFracc['descripcion']?></textarea>
+                            <label for="icon_prefix2">Descripcion de la Fraccion</label>
+                        </div> 
+
+                        <div align="center" class="col l12" style="align-items: center;">
+                            <!--input type="submit" value="Enviar" class="form-button" /-->
+                            <button class="btn waves-effect waves-light col l12" type="submit" name="Submitmodi" >Enviar
+                                <i class="material-icons "></i>
+                              </button>
+                        </div>
+                </form>
+
+                            <iframe name="updh" height="0" width="0" ></iframe>
+            </div>
+
+
